2e0828be1593aabc528a00d13dcfddf450c91028
tuongroth <44430600+tuongroth@users.noreply.github.com>
2024-06-24 20:18:56 +0300

    Update Person.js

--- part2/phonebook/components/Person.js
+++ part2/phonebook/components/Person.js
@@ -1,1 +1,18 @@
+import React from 'react';
+
+const Person = ({ person, deletePerson }) => {
+  const handleDelete = () => {
+    deletePerson(person.id); // Assuming person.id is the unique identifier
+  };
+
+  return (
+    <div className="person">
+      <p>Name: {person.name}</p>
+      <p>Number: {person.number}</p>
+      <button onClick={handleDelete}>Delete</button>
+    </div>
+  );
+};
+
+export default Person;
 
