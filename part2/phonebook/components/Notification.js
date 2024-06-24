f3a06a9c67d1b1d893dc7d782b77bee7eb6756be
tuongroth <44430600+tuongroth@users.noreply.github.com>
2024-06-24 20:20:11 +0300

    Update Notification.js

--- part2/phonebook/components/Notification.js
+++ part2/phonebook/components/Notification.js
@@ -1,1 +1,16 @@
 
+import React from 'react';
+
+const Notification = ({ message, error = false }) => {
+  if (!message) {
+    return null;
+  }
+
+  return (
+    <div className={error ? 'error' : 'message'}>
+      {message}
+    </div>
+  );
+};
+
+export default Notification;
