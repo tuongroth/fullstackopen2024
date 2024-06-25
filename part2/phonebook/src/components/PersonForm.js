55e95d6be571e55f34f72b1158f722ec346d6813
tuongroth <44430600+tuongroth@users.noreply.github.com>
2024-06-25 18:07:52 +0300

    Update PersonForm.js

--- part2/phonebook/src/components/PersonForm.js
+++ part2/phonebook/src/components/PersonForm.js
@@ -1,1 +1,17 @@
+import React from 'react';
 
+const PersonForm = ({ handleSubmit, newName, handleNameChange, newNumber, handleNumberChange }) => {
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        Name: <input value={newName} onChange={handleNameChange} />
+      </div>
+      <div>
+        Number: <input value={newNumber} onChange={handleNumberChange} />
+      </div>
+      <button type="submit">Add</button>
+    </form>
+  );
+};
+
+export default PersonForm;
