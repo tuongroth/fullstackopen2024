bb792554fcc20f2808e3bb5f4840db3087082bcc
tuongroth <44430600+tuongroth@users.noreply.github.com>
2024-06-25 18:47:27 +0300

    Update Content.js

--- part2/courseinfo/src/components/Content.js
+++ part2/courseinfo/src/components/Content.js
@@ -1,14 +1,1 @@
-import React from 'react';
-import Part from './Part';
 
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map(part => (
-        <Part key={part.id} part={part} />
-      ))}
-    </div>
-  );
-};
-
-export default Content;
