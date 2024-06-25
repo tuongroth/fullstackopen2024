92556c83b2b7550928d7efbb7d1767c8c0c88f80
tuongroth <44430600+tuongroth@users.noreply.github.com>
2024-06-25 19:04:14 +0300

    Update Header.js

--- part2/courseinfo/src/components/Header.js
+++ part2/courseinfo/src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const Header = ({ course }) => {
-  return <h1>{course}</h1>;
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
 };
 
 export default Header;
