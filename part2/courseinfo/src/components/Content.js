72ec81dce2fb2dd3d7521dd8f1e575216fe6435f
tuongroth <44430600+tuongroth@users.noreply.github.com>
2024-06-25 19:25:13 +0300

    Update Content.js

--- part2/courseinfo/src/components/Content.js
+++ part2/courseinfo/src/components/Content.js
@@ -1,1 +1,12 @@
+import Part from './Part'
 
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part, i) => (
+        <Part key={i} part={part.name} exercises1={part.exercises} />
+      ))}
+    </div>
+  );
+};
+export default Content
