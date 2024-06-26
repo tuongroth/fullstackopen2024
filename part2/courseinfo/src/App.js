3e9a634420306fa22eca61a09c64066e4d08d791
tuongroth <44430600+tuongroth@users.noreply.github.com>
2024-06-26 11:45:44 +0300

    Update App.js

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,34 +1,38 @@
+
 import React from 'react';
-import Course from './course';
+import ReactDOM from 'react-dom';
+import Course from './components/Course';
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  };
+  const courses = [
+    {
+      id: 1,
+      name: 'Half Stack application development',
+      parts: [
+        { id: 1, name: 'Fundamentals of React', exercises: 10 },
+        { id: 2, name: 'Using props to pass data', exercises: 7 },
+        { id: 3, name: 'State of a component', exercises: 14 }
+      ]
+    },
+    {
+      id: 2,
+      name: 'Node.js',
+      parts: [
+        { id: 1, name: 'Routing', exercises: 3 },
+        { id: 2, name: 'Middlewares', exercises: 7 }
+      ]
+    }
+  ];
 
   return (
     <div>
-      <Course course={course} />
+      {courses.map((course) => (
+        <Course key={course.id} course={course} />
+      ))}
     </div>
   );
 };
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 export default App;
