c536678ff3e5a546bb2886b1b2507e6bc60da7d8
tuongroth <44430600+tuongroth@users.noreply.github.com>
2024-06-26 09:32:04 +0300

    Update Content.js

--- part2/phonebook/src/components/Content.js
+++ part2/phonebook/src/components/Content.js
@@ -1,1 +1,15 @@
+import React from 'react';
+import Person from './Person';
+
+const Content = ({ personsToShow, deletePerson }) => {
+  return (
+    <ul>
+      {personsToShow.map(person => (
+        <Person key={person.id} person={person} deletePerson={deletePerson} />
+      ))}
+    </ul>
+  );
+};
+
+export default Content;
 
