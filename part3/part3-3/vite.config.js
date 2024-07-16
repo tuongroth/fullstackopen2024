a2d0d68c57a5068ab68c784a268d37feab59bc1a
tuongroth <44430600+tuongroth@users.noreply.github.com>
2024-07-16 13:29:59 +0300

    Update vite.config.js

--- part3/part3-3/vite.config.js
+++ part3/part3-3/vite.config.js
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  plugins: [react()],
   build: {
-    outDir: 'dist',   // Output directory for production build
-    assetsDir: '',    // Directory for assets like images or fonts
-    sourcemap: false, // Disable sourcemaps for production
-    minify: true      // Minify JavaScript, CSS, and HTML
+    outDir: 'dist',
+    rollupOptions: {
+      // additional configurations if needed
+    }
   }
-});
+})
