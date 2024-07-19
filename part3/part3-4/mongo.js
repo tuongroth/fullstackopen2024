307bf362a45edab836a14cbb834d2a421d534249
tuongroth <44430600+tuongroth@users.noreply.github.com>
2024-07-19 17:45:35 +0300

    Update mongo.js

--- part3/part3-4/mongo.js
+++ part3/part3-4/mongo.js
@@ -1,40 +1,68 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+
 
 if (process.argv.length < 3) {
-  console.log('give password as argument')
-  process.exit(1)
+  console.log('Usage: node mongo.js <password> [name] [number]');
+  process.exit(1);
 }
 
-const password = process.argv[2]
+const password = process.argv[2];
+const url = `mongodb+srv://fullstack:${password}@cluster0.xgr0xci.mongodb.net/phonebook?retryWrites=true&w=majority`;
 
-const url =
-  `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/noteApp?retryWrites=true&w=majority`
 
-mongoose.set('strictQuery', false)
-mongoose.connect(url).then(() => {
-  const noteSchema = new mongoose.Schema({
-    content: String,
-    important: Boolean,
+mongoose.connect(url)
+  .then(() => {
+    console.log('Connected to MongoDB');
+    performOperations();
   })
-  
-  const Note = mongoose.model('Note', noteSchema)
-  
-  const note = new Note({
-    content: 'HTML is x',
-    important: true,
-  })
-  
-  /*
-  note.save().then(result => {
-    console.log('note saved!')
-    mongoose.connection.close()
-  })
-  */
-  Note.find({}).then(result => {
-    result.forEach(note => {
-      console.log(note)
-    })
-    mongoose.connection.close()
-  })
-})
+  .catch(err => {
+    console.error('Error connecting to MongoDB:', err.message);
+  });
+
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+function performOperations() {
+  if (process.argv.length === 3) {
+    
+    Person.find({})
+      .then(persons => {
+        console.log('phonebook:');
+        persons.forEach(person => {
+          console.log(`${person.name} ${person.number}`);
+        });
+        mongoose.connection.close();
+      })
+      .catch(err => {
+        console.error(err);
+        mongoose.connection.close();
+      });
+  } else if (process.argv.length === 5) {
+    
+    const name = process.argv[3];
+    const number = process.argv[4];
+
+    const newPerson = new Person({ name, number });
+
+    newPerson.save()
+      .then(() => {
+        console.log(`added ${name} number ${number} to phonebook`);
+        mongoose.connection.close();
+      })
+      .catch(err => {
+        console.error(err);
+        mongoose.connection.close();
+      });
+  } else {
+    console.log('Usage: node mongo.js <password> [name] [number]');
+    mongoose.connection.close();
+  }
+}
+
+
 
