1a8b6ac1413c68159c0e1b3804279e5859764ccb
tuongroth <44430600+tuongroth@users.noreply.github.com>
2024-07-20 08:12:21 +0300

    Update person.js

--- part3/part3-4/model/person.js
+++ part3/part3-4/model/person.js
@@ -1,1 +1,32 @@
+const mongoose = require('mongoose')
 
+const url = process.env.MONGODB_URI
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB')
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error.message)
+  })
+
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  number: {
+    type: String,
+    required: true,
+  },
+})
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Person', personSchema)
