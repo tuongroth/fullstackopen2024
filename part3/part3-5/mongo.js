5107bf642e9268dbaa3675ffb232581841da115d
tuongroth <44430600+tuongroth@users.noreply.github.com>
2024-07-21 10:25:19 +0300

    Update mongo.js

--- part3/part3-5/mongo.js
+++ part3/part3-5/mongo.js
@@ -8,33 +8,37 @@ if (process.argv.length < 3) {
 const password = process.argv[2]
 
 const url =
-  `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/noteApp?retryWrites=true&w=majority`
+  `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/phonebook?retryWrites=true&w=majority`
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url).then(() => {
-  const noteSchema = new mongoose.Schema({
-    content: String,
-    important: Boolean,
+  const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
   })
   
-  const Note = mongoose.model('Note', noteSchema)
+  const Person = mongoose.model('Person', personSchema)
   
-  const note = new Note({
-    content: 'HTML is x',
-    important: true,
+  // Create a new person (example data)
+  const person = new Person({
+    name: 'John Doe',
+    number: '123-456-7890',
   })
   
   /*
-  note.save().then(result => {
-    console.log('note saved!')
+  person.save().then(result => {
+    console.log('person saved!')
     mongoose.connection.close()
   })
   */
-  Note.find({}).then(result => {
-    result.forEach(note => {
-      console.log(note)
+  
+  // Fetch all persons
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(person)
     })
     mongoose.connection.close()
   })
 })
+
 
