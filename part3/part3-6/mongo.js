ead379f831d27649c054d0c7f3b5ae02f42c79a4
tuongroth <44430600+tuongroth@users.noreply.github.com>
2024-07-22 22:53:26 +0300

    Update mongo.js

--- part3/part3-6/mongo.js
+++ part3/part3-6/mongo.js
@@ -1,47 +1,43 @@
 const mongoose = require('mongoose');
 
+if (process.argv.length < 3) {
+  console.log('give password as argument');
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url =
+  `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/noteApp?retryWrites=true&w=majority`;
+
 mongoose.set('strictQuery', false);
-
-const url = process.env.MONGODB_URI;
-
-console.log('connecting to', url);
-mongoose.connect(url)
-  .then(() => {
-    console.log('connected to MongoDB');
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message);
+mongoose.connect(url).then(() => {
+  const personSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      minlength: 3,
+      required: true
+    },
+    number: {
+      type: String,
+      required: true,
+      validate: {
+        validator: function(v) {
+          return /\d{2,3}-\d{6,}/.test(v);
+        },
+        message: props => `${props.value} is not a valid phone number!`
+      }
+    }
   });
 
-const personSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    minlength: 3,
-    required: true
-  },
-  number: {
-    type: String,
-    required: true,
-    validate: {
-      validator: function(v) {
-        return /\d{2,3}-\d{6,}/.test(v);
-      },
-      message: props => `${props.value} is not a valid phone number!`
-    }
-  }
+  const Person = mongoose.model('Person', personSchema);
+
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(person);
+    });
+    mongoose.connection.close();
+  });
+}).catch(error => {
+  console.error('error connecting to MongoDB:', error.message);
 });
-
-personSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  }
-});
-
-module.exports = mongoose.model('Person', personSchema);
-
-    mongoose.connection.close()
-  })
-})
-
