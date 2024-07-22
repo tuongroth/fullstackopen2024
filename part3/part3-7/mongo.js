5d347675daff56b52d1843c93c0ae7eaf68a9433
tuongroth <44430600+tuongroth@users.noreply.github.com>
2024-07-22 22:28:48 +0300

    Update mongo.js

--- part3/part3-7/mongo.js
+++ part3/part3-7/mongo.js
@@ -1,39 +1,43 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 if (process.argv.length < 3) {
-  console.log('give password as argument')
-  process.exit(1)
+  console.log('give password as argument');
+  process.exit(1);
 }
 
-const password = process.argv[2]
+const password = process.argv[2];
 
 const url =
-  `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/noteApp?retryWrites=true&w=majority`
+  `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/noteApp?retryWrites=true&w=majority`;
 
-mongoose.set('strictQuery', false)
+mongoose.set('strictQuery', false);
 mongoose.connect(url).then(() => {
-  const noteSchema = new mongoose.Schema({
-    content: String,
-    important: Boolean,
-  })
+  const personSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      minlength: 3,
+      required: true
+    },
+    number: {
+      type: String,
+      required: true,
+      validate: {
+        validator: function(v) {
+          return /\d{2,3}-\d{6,}/.test(v);
+        },
+        message: props => `${props.value} is not a valid phone number!`
+      }
+    }
+  });
 
-  const Note = mongoose.model('Note', noteSchema)
+  const Person = mongoose.model('Person', personSchema);
 
-  /*
-  const note = new Note({
-    content: 'HTML is x',
-    important: true,
-  })
-
-  note.save().then(result => {
-    console.log('note saved!')
-    mongoose.connection.close()
-  })
-  */
-  Note.find({}).then(result => {
-    result.forEach(note => {
-      console.log(note)
-    })
-    mongoose.connection.close()
-  })
-})
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(person);
+    });
+    mongoose.connection.close();
+  });
+}).catch(error => {
+  console.error('error connecting to MongoDB:', error.message);
+});
