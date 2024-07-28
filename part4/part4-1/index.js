775de7399555245098c8c4601bb2c8a5bf466837
tuongroth <44430600+tuongroth@users.noreply.github.com>
2024-07-28 13:14:05 +0300

    Update index.js

--- part4/part4-1/index.js
+++ part4/part4-1/index.js
@@ -2,19 +2,31 @@ const app = require('./app');
 const http = require('http');
 const config = require('./utils/config');
 const logger = require('./utils/logger');
-const connectToDatabase = require('./utils/database');
+const mongoose = require('mongoose');
 
-const server = http.createServer(app);
+mongoose.set('strictQuery', false);
+
+const connectToDatabase = async () => {
+  try {
+    logger.info('Connecting to MongoDB at', config.MONGODB_URI);
+    await mongoose.connect(config.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    logger.info('Successfully connected to MongoDB');
+  } catch (error) {
+    logger.error('Error connecting to MongoDB:', error.message);
+    process.exit(1); // Exit the process with failure
+  }
+};
 
 const startServer = async () => {
-  try {
-    await connectToDatabase();
-    server.listen(config.PORT, () => {
-      logger.info(`Server running on port ${config.PORT}`);
-    });
-  } catch (error) {
-    logger.error('Failed to start server:', error.message);
-  }
+  await connectToDatabase();
+  const server = http.createServer(app);
+
+  server.listen(config.PORT, () => {
+    logger.info(`Server running on port ${config.PORT}`);
+  });
 };
 
 startServer();
