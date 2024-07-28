ca2c5d5c772997432d98206188359aba22f89b6a
tuongroth <44430600+tuongroth@users.noreply.github.com>
2024-07-28 13:13:42 +0300

    Update app.js

--- part4/part4-1/app.js
+++ part4/part4-1/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
-const notesRouter = require('./controllers/notes');
+const cors = require('cors');
+const blogsRouter = require('./controllers/blogs');
 const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
 
+app.use(cors());
 app.use(express.json());
-app.use('/api/notes', notesRouter);
+
+app.use('/api/blogs', blogsRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
