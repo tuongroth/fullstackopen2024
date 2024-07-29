c76b721f179d19fa801b8611302041f9249bbdcf
tuongroth <44430600+tuongroth@users.noreply.github.com>
2024-07-29 11:30:29 +0300

    Update config.js

--- part4/part4-2/ulis/config.js
+++ part4/part4-2/ulis/config.js
@@ -1,9 +1,10 @@
-require('dotenv').config()
+// utils/config.js
+require('dotenv').config();
 
-const PORT = process.env.PORT || 3003
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/notesapp'
+const MONGODB_URI = process.env.MONGODB_URI;
+const PORT = process.env.PORT || 3003;
 
 module.exports = {
   MONGODB_URI,
-  PORT
-}
+  PORT,
+};
