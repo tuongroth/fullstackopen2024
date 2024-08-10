afaee8eab2d34f141ccac2f88c6935b67a845d3c
tuongroth <44430600+tuongroth@users.noreply.github.com>
2024-08-10 23:01:31 +0300

    Update mongo.js

--- part4/part4-9/mongo.js
+++ part4/part4-9/mongo.js
@@ -1,66 +1,44 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 if (process.argv.length < 3) {
-  console.log('give password as argument')
-  process.exit(1)
+  console.log('Give password as argument');
+  process.exit(1);
 }
 
-const password = process.argv[2]
+const password = process.argv[2];
 
-const url =
-  `mongodb+srv://fullstack:2r6FcH9cLQRdnXHJ@cluster0.o1opl.mongodb.net/noteApp?retryWrites=true&w=majority`
+const url = `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/blogApp?retryWrites=true&w=majority`;
 
-mongoose.set('strictQuery', false)
+mongoose.set('strictQuery', false);
 mongoose.connect(url).then(() => {
-  const noteSchema = new mongoose.Schema({
-    content: String,
-    important: Boolean,
-  })
+  const blogSchema = new mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number,
+  });
 
-  const Note = mongoose.model('Note', noteSchema)
+  const Blog = mongoose.model('Blog', blogSchema);
 
+  // Uncomment to add a new blog
   /*
-  const note = new Note({
-    content: 'HTML is x',
-    important: true,
-  })
+  const blog = new Blog({
+    title: 'Example Blog Title',
+    author: 'Author Name',
+    url: 'http://example.com',
+    likes: 10,
+  });
 
-  note.save().then(result => {
-    console.log('note saved!')
-    mongoose.connection.close()
-  })
+  blog.save().then(result => {
+    console.log('Blog saved!');
+    mongoose.connection.close();
+  });
   */
-  Note.find({}).then(result => {
-    result.forEach(note => {
-      console.log(note)
-    })
-    mongoose.connection.close()
-  })
-})
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://fullstack:2r6FcH9cLQRdnXHJ@cluster0.xgr0xci.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+  Blog.find({}).then(result => {
+    result.forEach(blog => {
+      console.log(blog);
+    });
+    mongoose.connection.close();
+  });
 });
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
