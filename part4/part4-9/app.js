a3e241d094d8a4b0b8468794a085f8afad8cec07
tuongroth <44430600+tuongroth@users.noreply.github.com>
2024-08-10 21:50:26 +0300

    Update app.js

--- part4/part4-9/app.js
+++ part4/part4-9/app.js
@@ -1,13 +1,37 @@
-const express = require('express');
-const app = express();
-const notesRouter = require('./controllers/notes'); // Assuming your notes routes are in `controllers/notes.js`
-const usersRouter = require('./controllers/users');
-const loginRouter = require('./controllers/login');
-const middleware = require('./utils/middleware');
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+require('express-async-errors')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const usersRouter = require('./controllers/users')
+const blogsRouter = require('./controllers/blogs')
+const loginRouter = require('./controllers/login')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
 
-// Use the middleware only in /api/notes routes
-app.use('/api/notes', middleware.userExtractor, notesRouter);
-app.use('/api/users', usersRouter);
-app.use('/api/login', loginRouter);
+mongoose.set('strictQuery', false)
 
-module.exports = app;
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.static('dist'))
+app.use(express.json())
+app.use(middleware.tokenExtractor) // Exercise 4.20: Token Extractor Middleware
+
+app.use('/api/users', usersRouter)
+app.use('/api/blogs', middleware.userExtractor, blogsRouter) // Exercise 4.22: User Extractor Middleware
+app.use('/api/login', loginRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
