0de3d6568173a91eb72627e1a70daf5f1ce7d44b
tuongroth <44430600+tuongroth@users.noreply.github.com>
2024-08-10 21:40:27 +0300

    Update blog.js

--- part4/part4-9/models/blog.js
+++ part4/part4-9/models/blog.js
@@ -1,19 +1,17 @@
 const mongoose = require('mongoose')
 
-const noteSchema = new mongoose.Schema({
-  content: {
-    type: String,
-    required: true,
-    minlength: 5
-  },
-  important: Boolean,
+const blogSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  author: String,
+  url: { type: String, required: true },
+  likes: { type: Number, default: 0 },
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User'
   }
 })
 
-noteSchema.set('toJSON', {
+blogSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
@@ -21,4 +19,5 @@ noteSchema.set('toJSON', {
   }
 })
 
-module.exports = mongoose.model('Note', noteSchema)
+const Blog = mongoose.model('Blog', blogSchema)
+module.exports = Blog
