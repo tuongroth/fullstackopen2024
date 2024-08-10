43ac566258036096364fc4adef9729b854db2120
tuongroth <44430600+tuongroth@users.noreply.github.com>
2024-08-10 21:48:36 +0300

    Create middlewarse.js

---
+++ part4/part4-9/utils/middlewarse.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    request.token = authorization.substring(7)
+  }
+  next()
+}
+
+// Exercise 4.22: UserExtractor Middleware
+const userExtractor = async (request, response, next) => {
+  const token = request.token
+  if (token) {
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (decodedToken.id) {
+      request.user = await User.findById(decodedToken.id)
+    }
+  }
+  next()
+}
+
+module.exports = {
+  tokenExtractor,
+  userExtractor
+}
