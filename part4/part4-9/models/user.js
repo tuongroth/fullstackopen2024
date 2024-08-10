cd7bf92eb367f0d6c261701370ec3b4250b37c70
tuongroth <44430600+tuongroth@users.noreply.github.com>
2024-08-10 22:32:33 +0300

    Update user.js

--- part4/part4-9/models/user.js
+++ part4/part4-9/models/user.js
@@ -1,7 +1,6 @@
-///Exercise 4.15 & 4.17: Create User Model//
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
@@ -9,24 +8,24 @@ const userSchema = mongoose.Schema({
   },
   name: String,
   passwordHash: String,
-  notes: [
+  blogs: [
     {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'Note'
+      ref: 'Blog'  // 
     }
   ],
-})
+});
 
 userSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
     // the passwordHash should not be revealed
-    delete returnedObject.passwordHash
+    delete returnedObject.passwordHash;
   }
-})
+});
 
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model('User', userSchema);
 
-module.exports = User
+module.exports = User;
