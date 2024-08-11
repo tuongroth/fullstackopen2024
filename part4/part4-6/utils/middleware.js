3b20df1ba2db66f09fa972f48345d05140e4f214
tuongroth <44430600+tuongroth@users.noreply.github.com>
2024-08-11 19:06:51 +0300

    Create middleware.js

---
+++ part4/part4-6/utils/middleware.js
@@ -0,0 +1,34 @@
+const logger = require('./logger');
+
+// Middleware for logging requests
+const requestLogger = (request, response, next) => {
+  logger.info('Method:', request.method);
+  logger.info('Path:  ', request.path);
+  logger.info('Body:  ', request.body);
+  logger.info('---');
+  next();
+};
+
+// Middleware for handling unknown endpoints
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+
+// Middleware for handling errors
+const errorHandler = (error, request, response, next) => {
+  logger.error(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
+
+module.exports = {
+  requestLogger,
+  unknownEndpoint,
+  errorHandler
+};
