18572ede53758caf5fd217e7ff6536de1b35e604
tuongroth <44430600+tuongroth@users.noreply.github.com>
2024-08-11 19:36:15 +0300

    Update mongo.js

--- part4/part4-5/mongo.js
+++ part4/part4-5/mongo.js
@@ -1,40 +1,43 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 if (process.argv.length < 3) {
-  console.log('give password as argument')
-  process.exit(1)
+  console.log('give password as argument');
+  process.exit(1);
 }
 
-const password = process.argv[2]
+const password = process.argv[2];
 
-const url =
-  `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/noteApp?retryWrites=true&w=majority`
+const url = `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/blogApp?retryWrites=true&w=majority`;
 
-mongoose.set('strictQuery', false)
+mongoose.set('strictQuery', false);
 mongoose.connect(url).then(() => {
-  const noteSchema = new mongoose.Schema({
+  const blogSchema = new mongoose.Schema({
+    title: String,
     content: String,
-    important: Boolean,
-  })
+    author: String,
+    likes: Number,
+  });
 
-  const Note = mongoose.model('Note', noteSchema)
+  const Blog = mongoose.model('Blog', blogSchema);
 
   /*
-  const note = new Note({
-    content: 'HTML is x',
-    important: true,
-  })
+  const blog = new Blog({
+    title: 'Sample Blog Title',
+    content: 'This is the content of the sample blog.',
+    author: 'Author Name',
+    likes: 0,
+  });
 
-  note.save().then(result => {
-    console.log('note saved!')
-    mongoose.connection.close()
-  })
+  blog.save().then(result => {
+    console.log('blog saved!');
+    mongoose.connection.close();
+  });
   */
-  Note.find({}).then(result => {
-    result.forEach(note => {
-      console.log(note)
-    })
-    mongoose.connection.close()
-  })
-})
 
+  Blog.find({}).then(result => {
+    result.forEach(blog => {
+      console.log(blog);
+    });
+    mongoose.connection.close();
+  });
+});
