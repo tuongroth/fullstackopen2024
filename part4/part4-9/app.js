732023129a5a2285299467ef69a1c06ba3d4bd00
tuongroth <44430600+tuongroth@users.noreply.github.com>
2024-08-11 17:54:12 +0300

    Update app.js

--- part4/part4-9/app.js
+++ part4/part4-9/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
-const blogsRouter = require('./controllers/blogs'); // Updated path
+const blogsRouter = require('./controllers/blogs'); // Updated to handle blogs
 const usersRouter = require('./controllers/users');
 const loginRouter = require('./controllers/login');
 const middleware = require('./utils/middleware');
+
+// Middleware to parse JSON bodies
+app.use(express.json());
 
 // Use the middleware only in /api/blogs routes
 app.use('/api/blogs', middleware.userExtractor, blogsRouter);
