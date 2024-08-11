fa34d4f5411f7ddb139a08d018f7040cea9470e1
tuongroth <44430600+tuongroth@users.noreply.github.com>
2024-08-11 19:18:46 +0300

    Update app.js

--- part4/part4-6/app.js
+++ part4/part4-6/app.js
@@ -1,13 +1,11 @@
-// app.js
-
 const express = require('express');
 const app = express();
-const notesController = require('./controllers/notes.js'); // Adjust the path as necessary
+const blogsController = require('./controllers/blogs'); // Adjust the path if necessary
 
 app.use(express.json()); // For parsing application/json
 
-// Define routes
-app.delete('/api/notes/:id', notesController.deleteNote);
-app.patch('/api/notes/:id', notesController.updateNote);
+// Define routes for blog posts
+app.delete('/api/blogs/:id', blogsController.deleteBlog);
+app.patch('/api/blogs/:id', blogsController.updateBlog);
 
 module.exports = app;
