7d5cadf545bcd79809e85a1156e273b2481aaa4d
tuongroth <44430600+tuongroth@users.noreply.github.com>
2024-08-11 17:39:21 +0300

    Update blogs.test.js

--- part4/part4-9/tests/blogs.test.js
+++ part4/part4-9/tests/blogs.test.js
@@ -1,127 +1,71 @@
-const { test, describe, beforeEach, afterAll } = require('node:test')
-const assert = require('node:assert')
-const supertest = require('supertest')
-const mongoose = require('mongoose')
-const app = require('../app') // Adjust the path to your Express app
-const Blog = require('../models/blog')
-const User = require('../models/user')
-const { initialBlogs, nonExistingId, blogsInDb, usersInDb } = require('./test_helper')
+const supertest = require('supertest');
+const mongoose = require('mongoose');
+const app = require('../app');
+const Blog = require('../models/blog'); // Changed from Note to Blog
+const User = require('../models/user');
+const helper = require('./test_helper');
 
-const api = supertest(app)
+const api = supertest(app);
+
+let token;
 
 beforeEach(async () => {
-  await Blog.deleteMany({})
-  await User.deleteMany({})
+    await Blog.deleteMany({}); // Changed from Note to Blog
+    await User.deleteMany({});
 
-  const user = new User({ username: 'testuser', passwordHash: 'hashedpassword' })
-  await user.save()
+    // Create a user and get a token
+    const user = new User({ username: 'testuser', passwordHash: 'passwordhash' });
+    await user.save();
 
-  const blogObjects = initialBlogs.map(blog => new Blog({ ...blog, user: user._id }))
-  const promiseArray = blogObjects.map(blog => blog.save())
-  await Promise.all(promiseArray)
-})
+    const loginResponse = await api
+        .post('/api/login')
+        .send({ username: 'testuser', password: 'passwordhash' });
+    
+    token = loginResponse.body.token;
+});
 
-afterAll(() => {
-  mongoose.connection.close()
-})
+describe('adding a new blog', () => {
 
-describe('Blog API', () => {
-  test('blogs are returned as JSON', async () => {
-    await api
-      .get('/api/blogs')
-      .expect(200)
-      .expect('Content-Type', /application\/json/)
-  })
+    test('succeeds with valid data and token', async () => {
+        const newBlog = {
+            title: 'A new blog', // Updated from content to title
+            author: 'Author Name', // Added author field
+            url: 'http://example.com', // Added url field
+            likes: 10 // Added likes field
+        };
 
-  test('there are the correct number of blogs', async () => {
-    const response = await api.get('/api/blogs')
-    assert.strictEqual(response.body.length, initialBlogs.length)
-  })
+        await api
+            .post('/api/blogs') // Changed endpoint from /api/notes to /api/blogs
+            .set('Authorization', `Bearer ${token}`)
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/);
 
-  test('a specific blog is within the returned blogs', async () => {
-    const response = await api.get('/api/blogs')
-    const contents = response.body.map(r => r.content)
-    assert.ok(contents.includes('React patterns'))
-  })
+        const blogsAtEnd = await helper.blogsInDb(); // Changed from notesInDb to blogsInDb
+        expect(blogsAtEnd).toHaveLength(1);
+        expect(blogsAtEnd[0].title).toBe(newBlog.title); // Updated from content to title
+    });
 
-  test('a valid blog can be added', async () => {
-    const user = await User.findOne({})
-    const newBlog = {
-      title: 'New Blog',
-      author: 'Author Name',
-      url: 'https://newblog.com/',
-      likes: 10,
-      user: user._id
-    }
+    test('fails with status code 401 Unauthorized if token is not provided', async () => {
+        const newBlog = {
+            title: 'A blog without token', // Updated from content to title
+            author: 'Author Name',
+            url: 'http://example.com',
+            likes: 10
+        };
 
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(201)
-      .expect('Content-Type', /application\/json/)
+        await api
+            .post('/api/blogs') // Changed endpoint from /api/notes to /api/blogs
+            .send(newBlog)
+            .expect(401);
 
-    const blogsAtEnd = await blogsInDb()
-    assert.strictEqual(blogsAtEnd.length, initialBlogs.length + 1)
+        const blogsAtEnd = await helper.blogsInDb(); // Changed from notesInDb to blogsInDb
+        expect(blogsAtEnd).toHaveLength(0);
+    });
 
-    const titles = blogsAtEnd.map(r => r.title)
-    assert.ok(titles.includes('New Blog'))
-  })
-
-  test('a blog without a title is not added', async () => {
-    const user = await User.findOne({})
-    const newBlog = {
-      author: 'Author Name',
-      url: 'https://newblog.com/',
-      likes: 10,
-      user: user._id
-    }
-
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(400)
-
-    const blogsAtEnd = await blogsInDb()
-    assert.strictEqual(blogsAtEnd.length, initialBlogs.length)
-  })
-
-  test('a blog can be deleted', async () => {
-    const blogsAtStart = await blogsInDb()
-    const blogToDelete = blogsAtStart[0]
-
-    await api
-      .delete(`/api/blogs/${blogToDelete.id}`)
-      .expect(204)
-
-    const blogsAtEnd = await blogsInDb()
-    assert.strictEqual(blogsAtEnd.length, initialBlogs.length - 1)
-
-    const titles = blogsAtEnd.map(r => r.title)
-    assert.ok(!titles.includes(blogToDelete.title))
-  })
-
-  test('a blog can be updated', async () => {
-    const blogsAtStart = await blogsInDb()
-    const blogToUpdate = blogsAtStart[0]
-
-    const updatedBlog = {
-      ...blogToUpdate,
-      likes: blogToUpdate.likes + 1
-    }
-
-    await api
-      .put(`/api/blogs/${blogToUpdate.id}`)
-      .send(updatedBlog)
-      .expect(200)
-      .expect('Content-Type', /application\/json/)
-
-    const blogsAtEnd = await blogsInDb()
-    const updatedBlogFromDb = blogsAtEnd.find(blog => blog.id === blogToUpdate.id)
-    assert.strictEqual(updatedBlogFromDb.likes, blogToUpdate.likes + 1)
-  })
-})
-
+    // More tests as needed
+});
 
 afterAll(() => {
-    mongoose.connection.close()
-})
+    mongoose.connection.close();
+});
