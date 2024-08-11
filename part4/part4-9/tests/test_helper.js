989c2b5d713a6d54ffcca137d58545ced5140964
tuongroth <44430600+tuongroth@users.noreply.github.com>
2024-08-11 17:41:32 +0300

    Update test_helper.js

--- part4/part4-9/tests/test_helper.js
+++ part4/part4-9/tests/test_helper.js
@@ -1,39 +1,40 @@
-const Blog = require('../models/blog') // Assuming you have a Blog model
-const User = require('../models/user')
+const Blog = require('../models/blog');
+const User = require('../models/user');
 
 const initialBlogs = [
   {
-    title: 'React patterns',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 7
+    title: 'HTML is easy',
+    author: 'John Doe',
+    url: 'http://example.com/html',
+    likes: 10
   },
   {
-    title: 'Node.js Best Practices',
-    author: 'Rising Ode',
-    url: 'https://nodejsbestpractices.com/',
-    likes: 5
+    title: 'JavaScript is awesome',
+    author: 'Jane Doe',
+    url: 'http://example.com/js',
+    likes: 20
   }
-]
+];
 
 const nonExistingId = async () => {
-  const blog = new Blog({ title: 'Temporary blog', author: 'Test Author', url: 'https://tempblog.com/', likes: 0 })
-  await blog.save()
-  await blog.deleteOne()
+  const blog = new Blog({ title: 'Temporary blog', author: 'Temp Author', url: 'http://example.com/temp', likes: 1 });
+  await blog.save();
+  await blog.deleteOne();
 
-  return blog._id.toString()
-}
+  return blog._id.toString();
+};
 
 const usersInDb = async () => {
-  const users = await User.find({})
-  return users.map(user => user.toJSON())
-}
+  const users = await User.find({});
+  return users.map(user => user.toJSON());
+};
 
 const blogsInDb = async () => {
-  const blogs = await Blog.find({})
-  return blogs.map(blog => blog.toJSON())
-}
+  const blogs = await Blog.find({});
+  return blogs.map(blog => blog.toJSON());
+};
 
 module.exports = {
   initialBlogs, nonExistingId, blogsInDb, usersInDb
-}
+};
+
