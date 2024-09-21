1ac220a7447be707db7e46345f394da4400cc24b
tuongroth <44430600+tuongroth@users.noreply.github.com>
2024-09-21 10:00:41 +0300

    Create anecdotes.js

---
+++ part 6/src/services/anecdotes.js
@@ -0,0 +1,22 @@
+// services/anecdotes.js
+import axios from 'axios'
+
+const baseUrl = 'http://localhost:3001/anecdotes'
+
+const getAll = async () => {
+  const response = await axios.get(baseUrl)
+  return response.data
+}
+
+const create = async (content) => {
+  const newAnecdote = { content, votes: 0 }
+  const response = await axios.post(baseUrl, newAnecdote)
+  return response.data
+}
+
+const updateVote = async (id, updatedAnecdote) => {
+  const response = await axios.put(`${baseUrl}/${id}`, updatedAnecdote)
+  return response.data
+}
+
+export default { getAll, create, updateVote }
