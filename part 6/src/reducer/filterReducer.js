6da8a188c1a96d844559aad3b924722a254818ca
tuongroth <44430600+tuongroth@users.noreply.github.com>
2024-09-21 10:02:12 +0300

    Create filterReducer.js

---
+++ part 6/src/reducer/filterReducer.js
@@ -0,0 +1,15 @@
+// reducers/filterReducer.js
+import { createSlice } from '@reduxjs/toolkit'
+
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState: '',
+  reducers: {
+    setFilter(state, action) {
+      return action.payload
+    }
+  }
+})
+
+export const { setFilter } = filterSlice.actions
+export default filterSlice.reducer
